bd74ff7bb00390909a4080089995892a7213ac76
TCKACHIKSIS <e79532157848@gmail.com>
2021-07-27 21:33:54 +1000

    начал работу

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+import sys
+import os
+pygame.init()
+
+
+def load_image(name, color_key=None):
+    fullname = os.path.join(name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as messange:
+        print('Cannot load messange', name)
+        raise SystemExit(messange)
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+forest_tileset = {
+    
+}
